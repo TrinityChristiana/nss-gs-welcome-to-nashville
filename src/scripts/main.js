25171ee3bb7a4a7590b5c63696c63cf2d601e9cf
Trinity Terry <trinitycterry@gmail.com>
2020-02-01 11:58:13 -0600

    Added semantic ui

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,16 @@ eventManager.itinerarySaveEvent();
 parksHtmlToDom();
 
 artsHtmlToDomFunction();
+
+// $('.ui.basic.modal').modal('show');
+
+$('.menu .browse')
+    .popup({
+        // inline: true,
+        hoverable: true,
+        position: 'bottom right',
+        delay: {
+            show: 300,
+            hide: 800
+        }
+    });
