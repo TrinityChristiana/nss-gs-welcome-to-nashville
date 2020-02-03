9a0d0e7a95648c6e4eec575d36809156c3575855
Trinity Terry <trinitycterry@gmail.com>
2020-02-03 10:22:31 -0600

    fixed js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -43,6 +43,6 @@ const myFunction = (x) => {
     }
 }
 
-var x = window.matchMedia("(max-width: 775px)")
+var x = window.matchMedia("(max-width: 775px)");
 myFunction(x); // Call listener function at run time
 x.addListener(myFunction); // Attach listener function on state changes
